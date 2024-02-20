3cd7c14309699ccadd44e295755b0e3ed18fa97d
Michael Bella <michael.bella@sofarocean.com>
2024-02-20 13:03:19 -0800

    Added comments to the example LoRa Network Client

--- example_client/LoRa_Network_Client.py
+++ example_client/LoRa_Network_Client.py
@@ -5,26 +5,40 @@ import sys
 import os
 
 if __name__ == "__main__":
-    sname = os.path.basename(__file__)
+    # Check that one command line argument was passed
     if len(sys.argv) != 2:
+        # Get the name of this file
+        sname = os.path.basename(__file__)
+        
+        # Print usage message
         print(f"Usage: ./{sname} server_ip_addr")
         sys.exit(1)
 
+    # User provided ip address
     ipaddr = sys.argv[1]
-    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as skt:
-        skt.connect((ipaddr, 2600))
-        skt.settimeout(0.01)
 
+    # Open a socket
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as skt:
+        # Connect to port 2600 of the user specified ip address 
+        skt.connect((ipaddr, 2600))
+        # Set a timeout for the socket reads
+        skt.settimeout(0.1)
+
+        # Buffer for incoming messages
         buff = ""
         while(True):
+            # Handle timeouts
             try:
-                rx = skt.recv(1).decode()
-                buff += rx
+                # Read bytes from socket into buffer
+                buff += skt.recv(4).decode()
             except socket.timeout as e:
+                # Ignore timed out socket reads
                 pass
 
+            # If the buffer contains a newline char then split the newest message from the rest.
             if "\n" in buff:
                 pkt,sep,buff = buff.partition("\n")
                 print(pkt)
             else:
+                # Go to the start of the loop ASAP if we haven't received a full packet yet.
                 continue
